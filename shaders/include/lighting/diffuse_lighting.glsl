833c88a0e8abad4dc79e8551c2ccc687fbfb268d
Jmayk <giorgio.candela2004@gmail.com>
2024-11-19 13:45:22 +0100

    Ad Astra support

--- shaders/include/lighting/diffuse_lighting.glsl
+++ shaders/include/lighting/diffuse_lighting.glsl
@@ -102,7 +102,7 @@ vec3 get_diffuse_lighting(
 	vec3  lighting = vec3(0.0);
 	float directional_lighting = (0.9 + 0.1 * normal.x) * (0.8 + 0.2 * abs(flat_normal.y)); // Random directional shading to make faces easier to distinguish
 
-#if defined WORLD_OVERWORLD || defined WORLD_END
+#if defined WORLD_OVERWORLD || defined WORLD_END || defined WORLD_SPACE
 
 	// Sunlight/moonlight
 
@@ -110,6 +110,10 @@ vec3 get_diffuse_lighting(
 	vec3 diffuse = vec3(lift(max0(NoL), 0.33 * rcp(SHADING_STRENGTH)) * (1.0 - 0.5 * material.sss_amount));
 	vec3 bounced = 0.033 * (1.0 - shadows) * (1.0 - 0.1 * max0(normal.y)) * pow1d5(ao + eps) * pow4(light_levels.y) * BOUNCED_LIGHT_I;
 	vec3 sss = sss_approx(material.albedo, material.sss_amount, material.sheen_amount, mix(sss_depth, 0.0, shadow_distance_fade), LoV, shadows.x);
+
+	#ifdef WORLD_SPACE
+	bounced *= clamp01(smoothstep(0.0, 0.1, light_dir.y));
+	#endif
 
 	// Adjust SSS outside of shadow distance
 	sss *= mix(1.0, ao * (clamp01(NoL) * 0.9 + 0.1), clamp01(shadow_distance_fade));
